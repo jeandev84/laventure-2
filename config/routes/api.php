1ae63a6832ab8973d5cc672213e2e9ecd59fbc5c
jeandev84 <jeandev84@yahoo.com>
2022-04-08 03:32:17 +0300

    generate api controller and routes

--- config/routes/api.php
+++ config/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use Laventure\Foundation\Facade\Routing\Route;
 
 
 /*
@@ -8,3 +8,25 @@
 |----------------------------------------------------------------------
 */
 
+
+$api_product = [
+    "module" => "Api\\",
+    "prefix" => "api/",
+    "name" => "api."
+];
+
+Route::api()->group(function () {
+    Route::resourceAPI('product', 'ProductController');
+}, $api_product);
+
+
+
+$api_features_product = [
+    "module" => "Api\Features\\",
+    "prefix" => "api/features/",
+    "name" => "api.features."
+];
+
+Route::api()->group(function () {
+    Route::resourceAPI('features_product', 'ProductController');
+}, $api_features_product);
