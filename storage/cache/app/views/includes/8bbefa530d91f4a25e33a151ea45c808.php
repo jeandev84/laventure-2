c38afef3f5c86a7d0317248445bf66920ab3f553
jeandev84 <jeandev84@yahoo.com>
2022-04-14 21:54:59 +0300

    hf

---
+++ storage/cache/app/views/includes/8bbefa530d91f4a25e33a151ea45c808.php
@@ -0,0 +1,4 @@
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, eum.
+<p>
+    Bla Bla include path ( inc/page.php )
+</p>
--- template/views/index.php
+++ template/views/index.php
@@ -1,3 +1,5 @@
 <h1>Home</h1>
 
 <img src="{{ asset('/assets/images/laventure.png') }}" alt="laventure.png" width="200">
+
+{% includePath('inc/page.php') %}
